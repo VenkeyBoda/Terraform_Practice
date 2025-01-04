614c4db4323b3db31eaa974e5a48498f8e87657f
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2025-01-04 18:21:07 +0530

    added basic

--- Azure/Hello-tf/main.tf
+++ Azure/Hello-tf/main.tf
@@ -1,12 +1,6 @@
-# Aws Provider source and version being used
+# Azure Provider source and version being used   
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.82.2"
-    }
-
-# Azure Provider source and version being used    
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.14.0"
@@ -14,25 +8,11 @@ terraform {
   }
 }
 
-# Configure the aws Azure Provider
-provider "aws" {
-  region = "ap-south-1"
-}
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {   
     }
     subscription_id = "378c0d57-dbfd-4234-a90e-e479534fd83d"
-}
-
-# Create a aws resource group
-resource "aws_vpc" "net" {
-    cidr_block = "10.10.0.0/16"
-    tags = {
-      Name = "Hello-terraform"
-    }
-
 }
 
 # Create a azure resource group
