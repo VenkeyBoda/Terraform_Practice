6ab869d80b35ed7c02bc73edae21c4f32e63ca67
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2025-02-13 14:48:11 +0530

    changes

--- AWS/EC2-instance/main.tf
+++ AWS/EC2-instance/main.tf
@@ -1,9 +1,9 @@
 # importing an existing key pair the public key resource
-resource "aws_key_pair" "sshkey" {
-  key_name   = var.key-info.name
-  public_key = file(var.key-info.public_key_path)
+# resource "aws_key_pair" "sshkey" {
+#   key_name   = var.key-info.name
+#   public_key = file(var.key-info.public_key_path)
 
-}
+# }
 
 # using data sources for executing the results
 data "aws_ami" "webimage" {
@@ -21,7 +21,7 @@ resource "aws_instance" "web" {
   count                       = var.instance_count
   ami                         = data.aws_ami.webimage.id
   instance_type               = var.web_instance_type
-  key_name                    = aws_key_pair.sshkey.key_name
+  key_name                    = "id_rsa"
   associate_public_ip_address = var.instance_associate_public_ip_address
   tags = {
     Name = "${var.web_instance_name}-${count.index}"
@@ -31,24 +31,4 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [var.vpc_security_group_ids]
 }
 
-# # The primary use-case for the null resource is as a do-nothing containerfor arbitrary actions taken by a provisioner
-# resource "null_resource" "web_null" {
-#   count = var.instance_count
-#   triggers = {
-#     build_id = var.build_id
-#   }
-
-# # Establishes connection to be used by all
-#   connection {
-#     type        = "ssh"
-#     user        = var.instance_username
-#     private_key = file(var.key-info.private_key_path)
-#     host        = aws_instance.web[count.index].public_ip
-#   }
-# # generic remote provisioners (i.e. file/remote-exec)
-#   provisioner "remote-exec" {
-#     script = var.script
-#   }
-
-# }
 
