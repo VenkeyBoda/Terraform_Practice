52661717a3349bb73f619385f8ecef40bac06348
venkeyboda07@gmail.com <venkeyboda02@gmail.com>
2025-12-30 13:16:23 +0530

    added the eks cluster

---
+++ AWS/EKS_CLUSTER/README.md
@@ -0,0 +1,5 @@
+# Connect kubectl to your EKS cluster
+
+aws eks update-kubeconfig \
+  --region ap-south-1 \
+  --name venkat_aws-cluster
--- AWS/EKS_CLUSTER/main.tf
+++ AWS/EKS_CLUSTER/main.tf
@@ -1,102 +1,175 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+provider "aws" {
+  region = "ap-south-1"
+}
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
+resource "aws_vpc" "venkat_aws_vpc" {
+  cidr_block = "10.0.0.0/16"
 
-    actions = ["sts:AssumeRole"]
+  tags = {
+    Name = "venkat_aws-vpc"
   }
 }
 
-resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+resource "aws_subnet" "venkat_aws_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.venkat_aws_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.venkat_aws_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  map_public_ip_on_launch = true
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-#get vpc data
-data "aws_vpc" "default" {
-  default = true
-}
-#get public subnets for cluster
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "venkat_aws-subnet-${count.index}"
   }
 }
-#cluster provision
-resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLUSTER"
-  role_arn = aws_iam_role.example.arn
+
+resource "aws_internet_gateway" "venkat_aws_igw" {
+  vpc_id = aws_vpc.venkat_aws_vpc.id
+
+  tags = {
+    Name = "venkat_aws-igw"
+  }
+}
+
+resource "aws_route_table" "venkat_aws_route_table" {
+  vpc_id = aws_vpc.venkat_aws_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.venkat_aws_igw.id
+  }
+
+  tags = {
+    Name = "venkat_aws-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.venkat_aws_subnet[count.index].id
+  route_table_id = aws_route_table.venkat_aws_route_table.id
+}
+
+resource "aws_security_group" "venkat_aws_cluster_sg" {
+  vpc_id = aws_vpc.venkat_aws_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "venkat_aws-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "venkat_aws_node_sg" {
+  vpc_id = aws_vpc.venkat_aws_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "venkat_aws-node-sg"
+  }
+}
+
+resource "aws_eks_cluster" "venkat_aws" {
+  name     = "venkat_aws-cluster"
+  role_arn = aws_iam_role.venkat_aws_cluster_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids         = aws_subnet.venkat_aws_subnet[*].id
+    security_group_ids = [aws_security_group.venkat_aws_cluster_sg.id]
   }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-  ]
 }
 
-resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example1.name
-}
-
-#create node group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
-  node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.public.ids
+resource "aws_eks_node_group" "venkat_aws" {
+  cluster_name    = aws_eks_cluster.venkat_aws.name
+  node_group_name = "venkat_aws-node-group"
+  node_role_arn   = aws_iam_role.venkat_aws_node_group_role.arn
+  subnet_ids      = aws_subnet.venkat_aws_subnet[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
   }
-  instance_types = ["t2.medium"]
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  instance_types = ["t2.large"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.venkat_aws_node_sg.id]
+  }
+}
+
+resource "aws_iam_role" "venkat_aws_cluster_role" {
+  name = "venkat_aws-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "venkat_aws_cluster_role_policy" {
+  role       = aws_iam_role.venkat_aws_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "venkat_aws_node_group_role" {
+  name = "venkat_aws-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "venkat_aws_node_group_role_policy" {
+  role       = aws_iam_role.venkat_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "venkat_aws_node_group_cni_policy" {
+  role       = aws_iam_role.venkat_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "venkat_aws_node_group_registry_policy" {
+  role       = aws_iam_role.venkat_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
